bd499d87dae37c57267bbb8cd7de7543cd5dd936
Hardik981 <41016443+Hardik981@users.noreply.github.com>
2023-01-04 14:00:32 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [show, setShow] = useState(false);
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <button onClick={()=>setShow(!show)}>
+        {show ? 'Hide' : 'Show'}
+      </button>
+      <h1>{show ? 'Hi' : ''}</h1>
     </div>
   );
 }
